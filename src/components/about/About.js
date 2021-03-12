25d49f010bae7f0b34609812f61e5dc2e69d90f3
Fernando <fernandoehs@gmail.com>
2021-03-12 01:47:08 -0500

    about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,28 +1,70 @@
 import React from "react";
 import "./about.css";
 
+
+
 const About = () => {
   return (
     <div className="about-container">
-      
-          <div className="about-desc-title">
-        <h3>Let me tell you something about me</h3>
-          </div>
-        <div className="about-desc">
-        <p>
+       
+          
+     <div className="col-lg-6 col-md-6 col-sm-6 col-12">
+        <div className="about-desc service">
+           <div className= "icon">
+             <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+           </div>
+           <div className="about-desc-title">
+             <h3>Let me tell you something about me</h3>
+           </div>
+         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
           provident inventore quod voluptates sequi. Quis, necessitatibus omnis
           iusto perferendis dolores voluptate et dolorum inventore autem nemo,
           quos eveniet magni nesciunt.
-        </p>
+         </p>
       </div>
-      <div className="about-img">
-        <img
-          src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg"
-          alt="about"
-        />
+     </div>
+
+     
+    <div className="col-lg-6 col-md-6 col-sm-6 col-12">
+ 
+        <div className="about-desc service">
+           
+           <div className= "icon">
+           <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
+           </div>
+           <div className="about-desc-title">
+             <h3>Let me tell you something about me</h3>
+            </div>
+         <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
+          provident inventore quod voluptates sequi. Quis, necessitatibus omnis
+          iusto perferendis dolores voluptate et dolorum inventore autem nemo,
+          quos eveniet magni nesciunt.
+         </p>
       </div>
-    </div>
+   
+     </div>
+    
+     <div className="col-lg-6 col-md-6 col-sm-6 col-12">
+        <div className="about-desc service">
+           
+           <div className= "icon">
+           <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+           </div>
+           <div className="about-desc-title">
+             <h3>Let me tell you something about me</h3>
+            </div>
+         <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
+          provident inventore quod voluptates sequi. Quis, necessitatibus omnis
+          iusto perferendis dolores voluptate et dolorum inventore autem nemo,
+          quos eveniet magni nesciunt.
+         </p>
+      </div>
+     </div> 
+     </div>
+    
   );
 };
 
